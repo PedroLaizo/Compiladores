9bed9d50a01e8ec7181c8b15138d41a96969f650
Pedro Laizo <pedrolaizo@outlook.com>
2022-09-12 08:15:25 -0300

    .

---
+++ ufsj/edu/lox/Token.java
@@ -0,0 +1,20 @@
+package ufsj.edu.lox;
+
+public class Token {
+	TokenType type;
+	String lexeme;
+	Object literal;
+	int line;
+	
+	Token(TokenType type, String lexeme, Object literal, int line){
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line = line;
+	}
+	
+	public String toString() {
+		return type + " " + lexeme + " " + literal;
+	}
+
+}
---
+++ ufsj/edu/lox/TokenType.java
@@ -0,0 +1,16 @@
+package ufsj.edu.lox;
+
+ enum TokenType {
+	 //Tokens com 1 caractere
+	 LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+	 BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+	 
+	 //Coisas mais complexas
+	 AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+	 
+	 IDENTIFIER, NUMBER, STRING,  
+	 
+	 //Fim de arquivo
+	 EOF
+
+}
