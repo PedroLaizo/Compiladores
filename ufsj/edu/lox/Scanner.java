2e2fbc410e39063dcd7766f8b7aca4a7ed7274b7
Pedro Laizo <pedrolaizo@outlook.com>
2022-09-16 08:20:28 -0300

    .

--- ufsj/edu/lox/Scanner.java
+++ ufsj/edu/lox/Scanner.java
@@ -30,7 +30,7 @@ public class Scanner {
 	private boolean isAtEnd() {
 		return current >= source.length();
 	}
-	
+	 
 	private char advance() {
 		current++;
 		return source.charAt(current -1);
@@ -43,6 +43,54 @@ public class Scanner {
 	private void addToken(TokenType type, Object literal) {
 		String text = source.substring(start, current);
 		tokens.add(new Token(type, text, literal, line));
+	}
+	
+	private boolean match(char expected) {
+		if(isAtEnd()) return false;
+		if(source.charAt(current) != expected)
+			return false;
+		
+		current++;
+		return true;
+	}
+	
+	private char peek() {
+		if(isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+	
+	private void string() {
+		while (peek() != '"' && !isAtEnd()) {
+			if (peek() =='\n') line++;
+			advance();
+		}
+		if(isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+		advance();
+		String value = source.substring(start + 1, current -1);
+		addToken(STRING, value);
+	}
+	
+	private boolean isDigit(char c) {
+		return c >= '0' && c<='9';
+	}
+	
+	private void number() {
+		while (isDigit(peek())) advance();
+		if (peek() == '.' && isDigit(peekNext())) {
+			advance();
+			while (isDigit(peek())) advance();
+		}
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+	
+	private char peekNext() {
+		if (current +1 >= source.length())
+			return '\0';
+		
+		return source.charAt(current + 1);
 	}
 	
 	private void scanToken() {
@@ -58,8 +106,31 @@ public class Scanner {
 			case '+': addToken(PLUS); break;
 			case ';': addToken(SEMICOLON); break;
 			case '*': addToken(STAR); break;
+			case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+			case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+			case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+			case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+			case ' ':
+			case '\r':
+			case '\t':
+				break;
+			case '\n': line++; break;
+			case '/': 
+				if(match('/')) {
+					while (peek() != '\n' && !isAtEnd())
+						advance();
+				} else {
+					addToken(SLASH);
+				}
+			break;
+			case '"': string(); break;
 			default:
-				Lox.error(line, "Unexpected character");
+				if(isDigit(c)) {
+					number();
+				} else {
+					Lox.error(line, "Unexpected character");
+				}
+				break;
 		}
 	}
 
